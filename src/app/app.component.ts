e4b62770583d2b34f9ce74d1bbf67f634d293a6f
Wiktor Lemanski <wiktorlem01@gmail.com>
2022-12-16 10:10:39 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxXmlToJsonService } from 'ngx-xml-to-json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Library';
+  public jsonObj!: any;
+
+  constructor(private ngxXmlToJsonService: NgxXmlToJsonService) {
+    const options = {
+      // set up the default options
+      textKey: 'text', // tag name for text nodes
+      attrKey: 'attr', // tag for attr groups
+      cdataKey: 'cdata', // tag for cdata nodes (ignored if mergeCDATA is true)
+    };
+
+    fetch('./assets/czasopisma.xml')
+      .then((response) => response.text())
+      .then((data) => {
+        this.jsonObj = this.ngxXmlToJsonService.xmlToJson(data, options);
+
+        this.jsonObj = this.jsonObj.czasopisma.zmienne;
+        console.log(this.jsonObj);
+      })
+      .catch(console.error);
+  }
 }
